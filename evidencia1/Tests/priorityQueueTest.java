4cd774065f206dcd00d9fe625f72e927258111a5
Diego Arzate <122845220+Diego-Arzate-Torres@users.noreply.github.com>
2023-09-16 23:47:58 -0600

    Evidencia
    Si funca

---
+++ evidencia1/Tests/priorityQueueTest.java
@@ -0,0 +1,11 @@
+package evidencia1.Tests;
+
+import evidencia1.process.priorityQueue;
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+public class priorityQueueTest {
+
+
+}
