b28ee51b122a32e03699af44676b73c1f0df38b7
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-09-16 19:42:23 -0600

    Eryx 2.0

---
+++ src/Evidencia1/process/Menu.java
@@ -0,0 +1,8 @@
+package Evidencia1.process;
+
+public class Menu {
+    public Menu menu = new Menu();
+
+    public static String queueTop = "Establecer el tope de la cola.";
+    public static String
+}
---
+++ src/Evidencia1/ui/CLI.java
@@ -0,0 +1,8 @@
+package Evidencia1.ui;
+
+public class CLI {
+    public void startSimulation(){
+        System.out.println("Bienvenido al simulador.");
+
+    }
+}
