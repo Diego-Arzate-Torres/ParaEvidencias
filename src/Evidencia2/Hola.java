5eab86e2dc754d59b15e415a2178f55e0fc74b6a
Diego Arzate <122845220+Diego-Arzate-Torres@users.noreply.github.com>
2023-10-18 14:11:02 -0600

    Mimimimim, pinche mandon

--- src/Evidencia2/Hola.java
+++ src/Evidencia2/Hola.java
@@ -1,4 +1,10 @@
 package Evidencia2;
 
+import javax.swing.*;
+
 public class Hola {
+    public static void main(String[] args) {
+        JFrame JFrame = new JFrame();
+        JOptionPane.showMessageDialog(JFrame, "NO");
+    }
 }
