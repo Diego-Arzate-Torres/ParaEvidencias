3e14e1cd27084190c8ea300426d16d02d0738af9
Diego Arzate <122845220+Diego-Arzate-Torres@users.noreply.github.com>
2023-10-21 23:18:58 -0600

    Dios no pedi ser tu mejor guerrero

--- src/Evidencia2/SearchGraph.java
+++ src/Evidencia2/SearchGraph.java
@@ -3,24 +3,25 @@ package Evidencia2;
 import java.util.*;
 
 public class SearchGraph {
-    public static Grafo.Vertice breadthFirstSearch(Grafo grafo, String inicio, String objetivo) {
-        Queue<Grafo.Vertice> queue = new LinkedList<>();
-        Set<Grafo.Vertice> visited = new HashSet<>();
+    public static GrafoDijkstra.Vertice breadthFirstSearch(GrafoDijkstra grafo, String inicio, String objetivo) {
+        Queue<GrafoDijkstra.Vertice> queue = new LinkedList<>();
+        Set<GrafoDijkstra.Vertice> visited = new HashSet<>();
 
-        Grafo.Vertice start = GrafoDijkstra.getVertice(inicio);
+        GrafoDijkstra.Vertice start = grafo.getVertice(inicio);
         queue.add(start);
 
         while (!queue.isEmpty()) {
-            Grafo.Vertice current = queue.poll();
+            GrafoDijkstra.Vertice current = queue.poll();
             visited.add(current);
 
-            if (current.getNombre().equals(objetivo)) {
+            if (current.getName().equals(objetivo)) {
                 return current;
             }
 
-            for (Grafo.Vertice neighbor : grafo.getVecinos(current)) {
+            for (GrafoDijkstra.Arista arista : grafo.getAdyacencias(current)) {
+                GrafoDijkstra.Vertice neighbor = arista.getV2();
                 if (!visited.contains(neighbor)) {
-                    queue.add(neighbor);
+                    queue.add(grafo.getVertice(neighbor.getName()));
                 }
             }
         }
@@ -28,24 +29,25 @@ public class SearchGraph {
         return null;  // Retorna null si no se encuentra el objetivo
     }
 
-    public static Grafo.Vertice deepFirstSearch(Grafo grafo, String inicio, String objetivo) {
-        Stack<Grafo.Vertice> stack = new Stack<>();
-        Set<Grafo.Vertice> visited = new HashSet<>();
+    public static GrafoDijkstra.Vertice deepFirstSearch(GrafoDijkstra grafo, String inicio, String objetivo) {
+        Stack<GrafoDijkstra.Vertice> stack = new Stack<>();
+        Set<GrafoDijkstra.Vertice> visited = new HashSet<>();
 
-        Grafo.Vertice start = Grafo.getVertice(inicio);
+        GrafoDijkstra.Vertice start = grafo.getVertice(inicio);
         stack.push(start);
 
         while (!stack.isEmpty()) {
-            Grafo.Vertice current = stack.pop();
+            GrafoDijkstra.Vertice current = stack.pop();
             visited.add(current);
 
-            if (current.getNombre().equals(objetivo)) {
+            if (current.getName().equals(objetivo)) {
                 return current;
             }
 
-            for (Grafo.Vertice neighbor : grafo.getVecinos(current)) {
+            for (GrafoDijkstra.Arista arista : grafo.getAdyacencias(current)) {
+                GrafoDijkstra.Vertice neighbor = arista.getV2();
                 if (!visited.contains(neighbor)) {
-                    stack.push(neighbor);
+                    stack.push(grafo.getVertice(neighbor.getName()));
                 }
             }
         }
@@ -53,3 +55,4 @@ public class SearchGraph {
         return null;  // Retorna null si no se encuentra el objetivo
     }
 }
+
